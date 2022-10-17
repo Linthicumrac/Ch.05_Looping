479d773be9afb7664069230cbccc4d60f74ec809
linthicumrac <linthicumrac@urbandaleschools.com>
2022-10-17 10:22:14 -0500

    Updated 5.3 Camel Game (Still complete)

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -1,7 +1,7 @@
 import random
 '''
-CAMEL GAME
-----------
+CAMEL GAME 
+---------
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 '''
 print("HOW TO PLAY/ HOW TO NOT DIE!")
